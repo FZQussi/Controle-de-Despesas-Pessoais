7209fa82f1194dc5398143acca07348048491f8d
FZQussi <qussidev@gmail.com>
2026-01-06 09:55:18 +0000

    first commit

---
+++ controle-despesas/src/test/java/pt/com/despesas/AppTest.java
@@ -0,0 +1,19 @@
+package pt.com.despesas;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
